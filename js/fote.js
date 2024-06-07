084135ef9e35951bd74bd4b1560b74ffd85c99f5
stefanycarolline <stefanycarolline10@gmail.com>
2024-06-07 09:19:10 -0300

    iniciando o projeto

--- js/fote.js
+++ js/fote.js
@@ -1,6 +1,8 @@
-/* Aplicação da funcionalidade do JavaScript*/
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        document.body.className = button.id
-    })
+// Aplicação da funcionalidade do Javascript
+document.querySelectorAll('button').forEach(
+    button => {
+        button.addEventListener('click', () => {
+            document.body.className = button.id
+        })
+
 })
